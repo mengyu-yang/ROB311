1460b8daf893da974c51b39898d836c6b7b1ca22
mengyu-yang <my.yang@mail.utoronto.ca>
2020-03-18 21:29:09 -0400

    Upload working version

---
+++ A3 /decision_tree.py
@@ -0,0 +1,314 @@
+import numpy as np
+
+
+# DO NOT ADD TO OR MODIFY ANY IMPORT STATEMENTS
+
+
+def dt_entropy(goal, examples):
+    """
+    Compute entropy over discrete random varialbe for decision trees.
+    Utility function to compute the entropy (which is always over the 'decision'
+    variable, which is the last column in the examples).
+
+    :param goal: Decision variable (e.g., WillWait), cell array.
+    :param examples: Training data; the final class is given by the last column.
+    :return: Value of the entropy of the decision variable, given examples.
+    """
+    # INSERT YOUR CODE HERE.
+    entropy = 0.
+    # Be careful to check the number of examples
+    # Avoid NaN examples by treating the log2(0.0) = 0
+
+    # Get probabilities of each class based on training data
+    classes, counts = np.unique(examples[:, -1], return_counts=True)
+    class_prob = dict(zip(classes, counts / np.sum(counts)))
+
+    # Calculate entropy
+    for prob in class_prob.values():
+        entropy -= prob * np.log2(prob)
+
+    return entropy
+
+
+def dt_cond_entropy(attribute, col_idx, goal, examples):
+    """
+    Compute the conditional entropy for attribute. Utility function to compute the conditional entropy (which is always
+    over the 'decision' variable or goal), given a specified attribute.
+
+    :param attribute: Dataset attribute, cell array.
+    :param col_idx: Column index in examples corresponding to attribute.
+    :param goal: Decision variable, cell array.
+    :param examples: Training data; the final class is given by the last column.
+    :return: Value of the conditional entropy, given the attribute and examples.
+    """
+    # INSERT YOUR CODE HERE.
+    cond_entropy = 0.0
+
+    # Number of examples
+    N = examples.shape[0]
+
+    # Get count for each attribute value, which is equal to p_k + n_k
+    values, counts = np.unique(examples[:, col_idx], return_counts=True)
+    value_count = dict(zip(values, counts))
+
+    # Calculate cond_entropy
+    for value in value_count.keys():
+        # p_k = examples[(examples[:, -1] == 1) & (examples[:, col_idx] == value)].shape[0]
+        cond_entropy += value_count[value] / N * dt_entropy(goal, examples[(examples[:, col_idx] == value)])
+
+    return cond_entropy
+
+
+def dt_info_gain(attribute, col_idx, goal, examples):
+    """
+    Compute information gain for attribute.
+    Utility function to compute the information gain after splitting on attribute.
+
+    :param attribute: Dataset attribute, cell array.
+    :param col_idx: Column index in examples corresponding to attribute.
+    :param goal: Decision variable, cell array.
+    :param examples: Training data; the final class is given by the last column.
+    :return: Value of the information gain, given the attribute and examples.
+
+    """
+
+    info_gain = dt_entropy(goal, examples) - dt_cond_entropy(attribute, col_idx, goal, examples)
+
+    return info_gain
+
+
+def dt_intrinsic_info(attribute, col_idx, examples):
+    """
+    Compute the intrinsic information for attribute.
+    Utility function to compute the intrinsic information of a specified attribute.
+
+    :param attribute: Dataset attribute, cell array.
+    :param col_idx: Column index in examples corresponding to attribute.
+    :param examples: Training data; the final class is given by the last column.
+    :return: Value of the intrinsic information for the attribute and examples.
+    """
+    # INSERT YOUR CODE HERE.
+    # Be careful to check the number of examples
+    # Avoid NaN examples by treating the log2(0.0) = 0
+    intrinsic_info = 0.
+
+    # Get probability for each attribute value, which is equal to p_k + n_k / p + n
+    values, counts = np.unique(examples[:, col_idx], return_counts=True)
+    value_prob = dict(zip(values, counts / np.sum(counts)))
+
+    # Calculate intrinsic info
+    for prob in value_prob.values():
+        intrinsic_info -= prob * np.log2(prob)
+
+    return intrinsic_info
+
+
+def dt_gain_ratio(attribute, col_idx, goal, examples):
+    """
+    Compute information gain ratio for attribute.
+    Utility function to compute the gain ratio after splitting on attribute. Note that this is just the information
+    gain divided by the intrinsic information.
+    :param attribute: Dataset attribute, cell array.
+    :param col_idx: Column index in examples corresponding to attribute.
+    :param goal: Decision variable, cell array.
+    :param examples: Training data; the final class is given by the last column.
+    :return: Value of the gain ratio, given the attribute and examples.
+    """
+    # INSERT YOUR CODE HERE.
+    # Avoid NaN examples by treating 0.0/0.0 = 0.0
+    info = dt_intrinsic_info(attribute, col_idx, examples)
+    if info == 0.0:
+        return 0.0
+    else:
+        gain_ratio = dt_info_gain(attribute, col_idx, goal, examples) / info
+    return gain_ratio
+
+
+def learn_decision_tree(parent, attributes, goal, examples, score_fun):
+    """
+    Recursively learn a decision tree from training data.
+    Learn a decision tree from training data, using the specified scoring function to determine which attribute to split
+    on at each step. This is an implementation of the algorithm on pg. 702 of AIMA.
+
+    :param parent: Parent node in tree (or None if first call of this algorithm).
+    :param attributes: Attributes avaialble for splitting at this node.
+    :param goal: Goal, decision variable (classes/labels).
+    :param examples: Subset of examples that reach this point in the tree.
+    :param score_fun: Scoring function used (dt_info_gain or dt_gain_ratio)
+    :return: Root node of tree structure.
+    """
+    # YOUR CODE GOES HERE
+    # 1. Do any examples reach this point?
+    if examples.size == 0:
+        label = plurality_value(goal, parent.examples)
+        return TreeNode(parent, attributes, examples, True, label)
+
+    # 2. Or do all examples have the same class/label? If so, we're done!
+    classes = examples[:, -1]
+    if np.all(classes == classes[0]):
+        return TreeNode(parent, attributes, examples, True, classes[0])
+
+    # 3. No attributes left? Choose the majority class/label.
+    elif len(attributes) == 0:
+        label = plurality_value(goal, examples)
+        return TreeNode(parent, attributes, examples, True, label)
+
+    # 4. Otherwise, need to choose an attribute to split on, but which one? Use score_fun and loop over attributes!
+    # Get best scoring attribute in attributes list. If there is a tie, select the smallest index
+    else:
+        scores = np.array([])
+        for i in range(len(attributes)):
+            scores = np.append(scores, score_fun(attributes[i], i, goal, examples))
+        A = np.min(np.argmax(scores))
+        best_attr = attributes[A]
+
+        # Delete best scoring attribute from list
+        attributes = list(attributes)
+        del attributes[A]
+
+        # Create a new internal node using the best attribute
+        node = TreeNode(parent=parent, attribute=best_attr, examples=examples, is_leaf=False, label=0)
+
+        # Iterate through each value of the attribute
+        for i in range(len(best_attr[1])):
+            # Get all examples which contain this value
+            exs = examples[examples[:, A] == i].copy()
+
+            # If there exists examples with particular attribute value, delete the corresponding attribute column
+            if exs.size != 0:
+                exs = np.delete(exs, A, axis=1)
+
+            # Recurse and append to node.branches
+            subNode = learn_decision_tree(node, attributes, goal, exs, score_fun)
+            node.branches.append(subNode)
+        return node
+
+
+def plurality_value(goal: tuple, examples: np.ndarray) -> int:
+    """
+    Utility function to pick class/label from mode of examples (see AIMA pg. 702).
+    :param goal: Tuple representing the goal
+    :param examples: (n, m) array of examples, each row is an example.
+    :return: index of label representing the mode of example labels.
+    """
+    vals = np.zeros(len(goal[1]))
+
+    # Get counts of number of examples in each possible attribute class first.
+    for i in range(len(goal[1])):
+        vals[i] = sum(examples[:, -1] == i)
+
+    return np.argmax(vals)
+
+
+class TreeNode:
+    """
+    Class representing a node in a decision tree.
+    When parent == None, this is the root of a decision tree.
+    """
+
+    def __init__(self, parent, attribute, examples, is_leaf, label):
+        # Parent node in the tree
+        self.parent = parent
+        # Attribute that this node splits on
+        self.attribute = attribute
+        # Examples used in training
+        self.examples = examples
+        # Boolean representing whether this is a leaf in the decision tree
+        self.is_leaf = is_leaf
+        # Label of this node (important for leaf nodes that determine classification output)
+        self.label = label
+        # List of nodes
+        self.branches = []
+
+    def query(self, attributes: np.ndarray, goal, query: np.ndarray) -> (int, str):
+        """
+        Query the decision tree that self is the root of at test time.
+
+        :param attributes: Attributes available for splitting at this node
+        :param goal: Goal, decision variable (classes/labels).2
+        :param query: A test query which is a (n,) array of attribute values, same format as examples but with the final
+                      class label).
+        :return: label_val, label_txt: integer and string representing the label index and label name.
+        """
+        node = self
+        while not node.is_leaf:
+            b = node.get_branch(attributes, query)
+            node = node.branches[b]
+
+        return node.label, goal[1][node.label]
+
+    def get_branch(self, attributes: list, query: np.ndarray):
+        """
+        Find attributes in a set of attributes and determine which branch to use (return index of that branch)
+
+        :param attributes: list of attributes
+        :param query: A test query which is a (n,) array of attribute values.
+        :return:
+        """
+        for i in range(len(attributes)):
+            if self.attribute[0] == attributes[i][0]:
+                return query[i]
+        # Return None if that attribute can't be found
+        return None
+
+    def count_tree_nodes(self, root=True) -> int:
+        """
+        Count the number of decision nodes in a decision tree.
+        :param root: boolean indicating if this is the root of a decision tree (needed for recursion base case)
+        :return: number of nodes in the tree
+        """
+        num = 0
+        for branch in self.branches:
+            num += branch.count_tree_nodes(root=False) + 1
+        return num + root
+
+
+if __name__ == '__main__':
+    # Example use of a decision tree from AIMA's restaurant problem on page (pg. 698)
+    # Each attribute is a tuple of 2 elements: the 1st is the attribute name (a string), the 2nd is a tuple of options
+    a0 = ('Alternate', ('No', 'Yes'))
+    a1 = ('Bar', ('No', 'Yes'))
+    a2 = ('Fri-Sat', ('No', 'Yes'))
+    a3 = ('Hungry', ('No', 'Yes'))
+    a4 = ('Patrons', ('None', 'Some', 'Full'))
+    a5 = ('Price', ('$', '$$', '$$$'))
+    a6 = ('Raining', ('No', 'Yes'))
+    a7 = ('Reservation', ('No', 'Yes'))
+    a8 = ('Type', ('French', 'Italian', 'Thai', 'Burger'))
+    a9 = ('WaitEstimate', ('0-10', '10-30', '30-60', '>60'))
+    attributes = [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9]
+    # The goal is a tuple of 2 elements: the 1st is the decision's name, the 2nd is a tuple of options
+    goal = ('WillWait', ('No', 'Yes'))
+
+    # Let's input the training data (12 examples in Figure 18.3, AIMA pg. 700)
+    # Each row is an example we will use for training: 10 features/attributes and 1 outcome (the last element)
+    # The first 10 columns are the attributes with 0-indexed indices representing the value of the attribute
+    # For example, the leftmost column represents the attribute 'Alternate': 0 is 'No', 1 is 'Yes'
+    # Another example: the 3rd last column is 'Type': 0 is 'French', 1 is 'Italian', 2 is 'Thai', 3 is 'Burger'
+    # The 11th and final column is the label corresponding to the index of the goal 'WillWait': 0 is 'No', 1 is 'Yes'
+    examples = np.array([[1, 0, 0, 1, 1, 2, 0, 1, 0, 0, 1],
+                         [1, 0, 0, 1, 2, 0, 0, 0, 2, 2, 0],
+                         [0, 1, 0, 0, 1, 0, 0, 0, 3, 0, 1],
+                         [1, 0, 1, 1, 2, 0, 1, 0, 2, 1, 1],
+                         [1, 0, 1, 0, 2, 2, 0, 1, 0, 3, 0],
+                         [0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+                         [0, 1, 0, 0, 0, 0, 1, 0, 3, 0, 0],
+                         [0, 0, 0, 1, 1, 1, 1, 1, 2, 0, 1],
+                         [0, 1, 1, 0, 2, 0, 1, 0, 3, 3, 0],
+                         [1, 1, 1, 1, 2, 2, 0, 1, 1, 1, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+                         [1, 1, 1, 1, 2, 0, 0, 0, 3, 2, 1]])
+
+    # print(dt_intrinsic_info(a4,4,examples))
+    # Build your decision tree using dt_info_gain as the score function
+    tree = learn_decision_tree(None, attributes, goal, examples, dt_info_gain)
+    # Query the tree with an unseen test example: it should be classified as 'Yes'
+    test_query = np.array([0, 0, 1, 1, 2, 0, 0, 0, 2, 3])
+    _, test_class = tree.query(attributes, goal, test_query)
+    print("Result of query: {:}".format(test_class))
+
+    # Repeat with dt_gain_ratio:
+    tree_gain_ratio = learn_decision_tree(None, attributes, goal, examples, dt_gain_ratio)
+    # Query this new tree: it should also be classified as 'Yes'
+    _, test_class = tree_gain_ratio.query(attributes, goal, test_query)
+    print("Result of query with gain ratio as score: {:}".format(test_class))
---
+++ A3 /iterated_single_move_games.py
@@ -0,0 +1,292 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class SingleMoveGamePlayer(ABC):
+    """
+    Abstract base class for a symmetric, zero-sum single move game player.
+    """
+    def __init__(self, game_matrix: np.ndarray):
+        self.game_matrix = game_matrix
+        self.n_moves = game_matrix.shape[0]
+        super().__init__()
+
+    @abstractmethod
+    def make_move(self) -> int:
+        pass
+
+
+class IteratedGamePlayer(SingleMoveGamePlayer):
+    """
+    Abstract base class for a player of an iterated symmetric, zero-sum single move game.
+    """
+    def __init__(self, game_matrix: np.ndarray):
+        super(IteratedGamePlayer, self).__init__(game_matrix)
+
+    @abstractmethod
+    def make_move(self) -> int:
+        pass
+
+    @abstractmethod
+    def update_results(self, my_move, other_move):
+        """
+        This method is called after each round is played
+        :param my_move: the move this agent played in the round that just finished
+        :param other_move:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def reset(self):
+        """
+        This method is called in between opponents (forget memory, etc.)
+        :return:
+        """
+        pass
+
+
+class UniformPlayer(IteratedGamePlayer):
+    def __init__(self, game_matrix: np.ndarray):
+        super(UniformPlayer, self).__init__(game_matrix)
+
+    def make_move(self) -> int:
+        """
+
+        :return:
+        """
+        return np.random.randint(0, self.n_moves)
+
+    def update_results(self, my_move, other_move):
+        """
+        The UniformPlayer player does not use prior rounds' results during iterated games.
+        :param my_move:
+        :param other_move:
+        :return:
+        """
+        pass
+
+    def reset(self):
+        """
+        This method is called in between opponents (forget memory, etc.)
+        :return:
+        """
+        pass
+
+
+class FirstMovePlayer(IteratedGamePlayer):
+    def __init__(self, game_matrix: np.ndarray):
+        super(FirstMovePlayer, self).__init__(game_matrix)
+
+    def make_move(self) -> int:
+        """
+        Always chooses the first move
+        :return:
+        """
+        return 0
+
+    def update_results(self, my_move, other_move):
+        """
+        The FirstMovePlayer player does not use prior rounds' results during iterated games.
+        :param my_move:
+        :param other_move:
+        :return:
+        """
+        pass
+
+    def reset(self):
+        """
+        This method is called in between opponents (forget memory, etc.)
+        :return:
+        """
+        pass
+
+
+class CopycatPlayer(IteratedGamePlayer):
+    def __init__(self, game_matrix: np.ndarray):
+        super(CopycatPlayer, self).__init__(game_matrix)
+        self.last_move = np.random.randint(self.n_moves)
+
+    def make_move(self) -> int:
+        """
+        Always copies the last move played
+        :return:
+        """
+        return self.last_move
+
+    def update_results(self, my_move, other_move):
+        """
+        The CopyCat player simply remembers the opponent's last move.
+        :param my_move:
+        :param other_move:
+        :return:
+        """
+        self.last_move = other_move
+
+    def reset(self):
+        """
+        This method is called in between opponents (forget memory, etc.)
+        :return:
+        """
+        self.last_move = np.random.randint(self.n_moves)
+
+
+def play_game(player1, player2, game_matrix: np.ndarray, N: int = 1000) -> (int, int):
+    """
+
+    :param player1: instance of an IteratedGamePlayer subclass for player 1
+    :param player2: instance of an IteratedGamePlayer subclass for player 2
+    :param game_matrix: square payoff matrix
+    :param N: number of rounds of the game to be played
+    :return: tuple containing player1's score and player2's score
+    """
+    p1_score = 0.0
+    p2_score = 0.0
+    n_moves = game_matrix.shape[0]
+    legal_moves = set(range(n_moves))
+    for idx in range(N):
+        move1 = player1.make_move()
+        move2 = player2.make_move()
+        if move1 not in legal_moves:
+            print("WARNING: Player1 made an illegal move: {:}".format(move1))
+            if move2 not in legal_moves:
+                print("WARNING: Player2 made an illegal move: {:}".format(move2))
+            else:
+                p2_score += np.max(game_matrix)
+                p1_score -= np.max(game_matrix)
+            continue
+        elif move2 not in legal_moves:
+            print("WARNING: Player2 made an illegal move: {:}".format(move2))
+            p1_score += np.max(game_matrix)
+            p2_score -= np.max(game_matrix)
+            continue
+        player1.update_results(move1, move2)
+        player2.update_results(move2, move1)
+
+        p1_score += game_matrix[move1, move2]
+        p2_score += game_matrix[move2, move1]
+
+    return p1_score, p2_score
+
+
+class StudentAgent(IteratedGamePlayer):
+    """
+    The general strategy for this game playing agent is to first detect if the opponent is one of three predefined
+    agents: the first move agent, the copycat agent, or the goldfish agent. If it is, then we can always play an
+    optimal strategy against the opponent's. To detect the opponent's strategy, we record the first 5 moves of the
+    opponent and compare it to our own. During these first five moves, we just initially play a random strategy. Below
+    describes the optimal strategies against the three opponent strategies:
+
+    FIRST MOVE AGENT: see if every move the opponent made is the same. If so, we can simply keep playing the corresponding
+    winning move.
+
+    COPYCAT AGENT: see if the opponent's move is always our previous move. If so, we play the winning move corresponding
+    to our last move.
+
+    GOLDFISH AGENT: see if the opponent's move is always the corresponding winning move to our last move. If so, we
+    play the winning move to the winning move of our last move.
+
+    If the opponent uses none of the strategies above, then we resort back to playing the random strategy as before,
+    since this is the mixed strategy Nash equilibrium for a standard rock paper scissors game.
+    """
+    def __init__(self, game_matrix: np.ndarray):
+        """
+        Initialize your game playing agent. here
+        :param game_matrix: square payoff matrix for the game being played.
+        """
+        super(StudentAgent, self).__init__(game_matrix)
+        self.opp_prev_moves = []
+        self.my_prev_moves = []
+        self.gm = game_matrix
+        self.count = 0
+        self.n_moves = game_matrix.shape[0]
+
+    def make_move(self) -> int:
+        """
+        Play your move based on previous moves or whatever reasoning you want.
+        :return: an int in (0, ..., n_moves-1) representing your move
+        """
+
+        # Given a move, return the corresponding winning move
+        def get_winning(move):
+            return (move + self.n_moves + 1) % self.n_moves
+
+        # Function to detect if the opponent is using the goldfish agent
+        def is_goldfish():
+            me = self.my_prev_moves[:-1].copy()
+            opp = self.opp_prev_moves[1:].copy()
+            for i in range(len(me)):
+                if get_winning(me[i]) != opp[i]:
+                    return False
+            return True
+
+        # Play randomly in the first 5 moves while obtaining data on opponent's strategy
+        if self.count < 5:
+            return np.random.randint(0, self.n_moves)
+
+        else:
+            # First move agent
+            if len(set(self.opp_prev_moves)) == 1:
+                return get_winning(self.opp_prev_moves[0])
+
+            # Copycat agent
+            elif self.my_prev_moves[:-1] == self.opp_prev_moves[1:]:
+                return get_winning(self.my_prev_moves[-1])
+
+            # Goldfish agent
+            elif is_goldfish():
+                return get_winning(get_winning(self.my_prev_moves[-1]))
+
+            # None of the agents above, then play randomly
+            else:
+                return np.random.randint(0, self.n_moves)
+
+    def update_results(self, my_move, other_move):
+        """
+        Update your agent based on the round that was just played.
+        :param my_move:
+        :param other_move:
+        :return: nothing
+        """
+
+        # Store our move as well as the opponent's move and increment turn counter
+        self.opp_prev_moves.append(other_move)
+        self.my_prev_moves.append(my_move)
+        self.count += 1
+
+    def reset(self):
+        """
+        This method is called in between opponents (forget memory, etc.).
+        :return: nothing
+        """
+
+        # Reset move lists and counter when switching agents
+        self.my_prev_moves = []
+        self.opp_prev_moves = []
+        self.count = 0
+
+
+if __name__ == '__main__':
+    """
+    Simple test on standard rock-paper-scissors
+    The game matrix's row (first index) is indexed by player 1 (P1)'s move (i.e., your move)
+    The game matrix's column (second index) is indexed by player 2 (P2)'s move (i.e., the opponent's move)
+    Thus, for example, game_matrix[0, 1] represents the score for P1 when P1 plays rock and P2 plays paper: -1.0 
+    because rock loses to paper.
+    """
+    game_matrix = np.array([[0.0, -1.0, 1.0],
+                            [1.0, 0.0, -1.0],
+                            [-1.0, 1.0, 0.0]])
+    uniform_player = UniformPlayer(game_matrix)
+    first_move_player = FirstMovePlayer(game_matrix)
+    uniform_score, first_move_score = play_game(uniform_player, first_move_player, game_matrix)
+
+    print("Uniform player's score: {:}".format(uniform_score))
+    print("First-move player's score: {:}".format(first_move_score))
+
+    # Now try your agent
+    student_player = StudentAgent(game_matrix)
+    student_score, first_move_score = play_game(student_player, first_move_player, game_matrix)
+
+    print("Your player's score: {:}".format(uniform_score))
+    print("First-move player's score: {:}".format(first_move_score))
